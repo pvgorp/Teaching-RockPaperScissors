a6744445ca40a9de87becc733a96bb881637c5c5
Pieter Van Gorp <git@pietervangorp.com>
2016-09-17 11:02:15 +0200

    Restored V2 (2 player looping version). Moved to logic of the computer player to V3

--- RockPaperScissors/src/nl/tue/apom/l3/GameV3.java
+++ RockPaperScissors/src/nl/tue/apom/l3/GameV3.java
@@ -1,28 +1,31 @@
 package nl.tue.apom.l3;
 
 import java.util.Scanner;
+
+import nl.tue.apom.l3.ai.AutomaticPlayer;
+import nl.tue.apom.l3.ai.AutomaticPlayerV1;
 import nl.tue.apom.l3.items.*;
 
 /**
- * Very basic support for the rock/paper/scissors game: taking two player inputs
- * and indicating whether someone has one, continue doing so until you have a
- * winner.
+ * Reasonably realistic rock/paper/scissors game: taking one player input 
+ * and comparing it against a random computer pick until one of both wins.
  * 
  * @author pvgorp
  *
  */
-public class GameV2 {
+public class GameV3 {
 
   private enum GameResult {
     UNDECIDED, P1WON, P2WON, TIE
   }
 
   public static void main(String[] args) {
-    String howto = "Please enter twice one of {paper,rock,scissors} to play";
+    String howto = "Please enter once one of {paper,rock,scissors} to play"; // CHANGED
     System.out.println(howto);
     Scanner s = new Scanner(System.in);
     Item i1 = null;
     Item i2 = null;
+    AutomaticPlayer player2= new AutomaticPlayerV1(); // NEW 
     GameResult result = GameResult.UNDECIDED;
     int round= 1;
     do {
@@ -30,21 +33,20 @@ public class GameV2 {
       do {
         try {
           System.out.println("Player 1: please enter your choice");
-          i1 = ItemFactory.toItem(s.next());
-          System.out.println("Player 2: please enter your choice");
-          i2 = ItemFactory.toItem(s.next());
+          i1 = ItemFactory.toItem(s.next());          
+          i2 = player2.play(); // CHANGED
         } catch (InvalidInputException e) {
           System.out.println(e);
           System.out.println(howto);
         }
       } while (i1 == null || i2 == null);
-      System.out.println("Player 1 selected " + i1);
-      System.out.println("Player 2 selected " + i2);
+      System.out.println("You selected " + i1); // CHANGED
+      System.out.println("Your computer opponent selected " + i2); // CHANGED
       if (i1.beats(i2)) {
-        System.out.println("Player 1 wins");
+        System.out.println("You win");  // CHANGED
         result = GameResult.P1WON;
       } else if (i2.beats(i1)) {
-        System.out.println("Player 2 wins");
+        System.out.println("You loose");
         result = GameResult.P2WON;
       } else {
         System.out.println("Nobody wins");

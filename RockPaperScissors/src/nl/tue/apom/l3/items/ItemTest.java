c2a295208ae84c41f5816d8f81bf4bb0f0cd6b95
Pieter Van Gorp <git@pietervangorp.com>
2016-09-17 23:45:59 +0200

    Added UML for Items separately and switched to old JUnit style for now

--- RockPaperScissors/src/nl/tue/apom/l3/items/ItemTest.java
+++ RockPaperScissors/src/nl/tue/apom/l3/items/ItemTest.java
@@ -1,64 +1,64 @@
 package nl.tue.apom.l3.items;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class ItemTest {
 
-  final static Rock r1= new Rock();
-  final static Rock r2= new Rock();
-  
-  final static Scissors s1= new Scissors();
-  final static Scissors s2= new Scissors();
-  
-  final static Paper p1= new Paper();
-  final static Paper p2= new Paper();
+  final static Rock r1 = new Rock();
+  final static Rock r2 = new Rock();
+
+  final static Scissors s1 = new Scissors();
+  final static Scissors s2 = new Scissors();
+
+  final static Paper p1 = new Paper();
+  final static Paper p2 = new Paper();
 
   @Test
-  public void testRockRock() {
-    assertThat(r1.beats(r2), equalTo(false)); 
+  public void testRockRock() {    
+    assertFalse(r1.beats(r2));//assertThat(r1.beats(r2), equalTo(false));
   }
-  
+
   @Test
-  public void testRockScissors() {
-    assertThat(r1.beats(s1), equalTo(true)); 
+  public void testRockScissors() {    
+    assertTrue(r1.beats(s1));//assertThat(r1.beats(s1), equalTo(true));
   }
-  
+
   @Test
   public void testRockPaper() {
-    assertThat(r1.beats(p1), equalTo(false)); 
+    //assertThat(r1.beats(p1), equalTo(false));
+    assertFalse(r1.beats(p1));
   }
-  
+
   @Test
   public void testScissorsScissors() {
-    assertThat(s1.beats(s1), equalTo(false)); 
+    assertFalse(s1.beats(s1));//assertThat(s1.beats(s1), equalTo(false));
   }
 
   @Test
   public void testScissorsPaper() {
-    assertThat(s1.beats(p1), equalTo(true)); 
+    assertTrue(s1.beats(p1));//assertThat(s1.beats(p1), equalTo(true));
   }
-  
+
   @Test
   public void testScissorsRock() {
-    assertThat(s1.beats(r1), equalTo(false)); 
+    assertFalse(s1.beats(r1));//assertThat(s1.beats(r1), equalTo(false));
   }
-  
+
   @Test
   public void testPaperPaper() {
-    assertThat(p1.beats(p2), equalTo(false)); 
+    assertFalse(p1.beats(p2));//assertThat(p1.beats(p2), equalTo(false));
   }
-  
+
   @Test
   public void testPaperScissors() {
-    assertThat(p1.beats(s1), equalTo(false)); 
+    assertFalse(p1.beats(s1));//assertThat(p1.beats(s1), equalTo(false));
   }
-  
+
   @Test
   public void testPapeRock() {
-    assertThat(p1.beats(r1), equalTo(true)); 
+    assertTrue(p1.beats(r1));//assertThat(p1.beats(r1), equalTo(true));
   }
 }
-

4d6e6846ed096768af40068cdc271b44025995bf
Pieter Van Gorp <git@pietervangorp.com>
2016-09-17 10:50:34 +0200

    automatic player v1

---
+++ RockPaperScissors/src/nl/tue/apom/l3/ai/AutomaticPlayer.java
@@ -0,0 +1,7 @@
+package nl.tue.apom.l3.ai;
+
+import nl.tue.apom.l3.items.Item;
+
+public interface AutomaticPlayer {
+  public Item play();
+}
---
+++ RockPaperScissors/src/nl/tue/apom/l3/ai/AutomaticPlayerV1.java
@@ -0,0 +1,22 @@
+package nl.tue.apom.l3.ai;
+
+import nl.tue.apom.l3.items.*;
+
+public class AutomaticPlayerV1 implements AutomaticPlayer {
+  java.util.Random ran= new java.util.Random();
+  final static double oneThird= 1.0/3.0;
+  final static double twoThird= 2.0/3.0;
+  final static double threeThird= 1.0;
+  
+  @Override
+  public Item play() {
+    double num= ran.nextDouble();
+    if (num < oneThird) {
+      return new Rock();
+    } else if (num < twoThird) {
+      return new Scissors();
+    } else {
+      return new Paper();
+    }
+  }
+}
